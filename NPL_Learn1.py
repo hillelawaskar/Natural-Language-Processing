818cc46bb1e3c41216dff734a33ccdb3fa7f2b83
hillelawaskar <hillel.awaskar@gmail.com>
2019-02-10 16:05:55 +0530

    updated Learn 1 file

---
+++ NPL_Learn1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 00:37:51 2019
+
+@author: Irene
+"""
+
+from nltk.book import *
+
+text1.concordance("Genesis")
+text1.similar("monstrous")
+
+text2.similar('monstrous')
