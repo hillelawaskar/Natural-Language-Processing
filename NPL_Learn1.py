30e8b832665144808160eff5fb83ed61740b5ad4
hillelawaskar <hillel.awaskar@gmail.com>
2019-02-10 18:31:13 +0530

    Updated NLP file

--- NPL_Learn1.py
+++ NPL_Learn1.py
@@ -2,12 +2,52 @@
 """
 Created on Sat Feb  9 00:37:51 2019
 
-@author: Irene
+@author: Hillel Awaskar
 """
+import nltk
+from __future__ import division
 
-from nltk.book import *
 
-text1.concordance("Genesis")
-text1.similar("monstrous")
+## the text is scrapped and collected from WikiPedia 
+Mauryan = open('D:\Data\CodePython\DataAI\Config\\Mauryanempire.txt', 'r',encoding="utf-8")
+Mau =  Mauryan.read()
+tokens = nltk.word_tokenize(Mau)
+Mauryan_text = nltk.Text(tokens)
 
-text2.similar('monstrous')
+
+
+### finding text 
+Mauryan_text.concordance("history") 
+Mauryan_text.concordance("chanakya") 
+
+Mauryan_text.dispersion_plot(["history","chanakya"])
+
+Mauryan_text.similar("history")
+
+Mauryan_text.common_contexts(["history"])
+
+len(Mauryan_text)
+
+sorted(set(Mauryan_text))
+
+len(set(Mauryan_text))
+
+
+len(Mauryan_text) / len(set(Mauryan_text))
+
+
+def lexical_diversity(text): 
+    return len(text) / len(set(text))
+
+def percentage(count, total): 
+    return 100 * count / total
+
+
+lexical_diversity(Mauryan_text) 
+percentage(Mauryan_text.count('history'), len(Mauryan_text)) 
+
+Mauryan_text[2000:2005]
+
+
+
+
