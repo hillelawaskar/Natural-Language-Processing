1068290c9f1906b7e200446ef36c715387f26862
hillelawaskar <hillel.awaskar@gmail.com>
2019-02-15 00:04:25 +0530

    Added code

--- NLP_Learn5_text_corpuses.py
+++ NLP_Learn5_text_corpuses.py
@@ -104,6 +104,15 @@ reuters.words(categories=['barley', 'corn'])
 
 ### Inaugral Corpous
 from nltk.corpus import inaugural
+inaugural.fileids()
+[fileid[:4] for fileid in inaugural.fileids()]
 
-
+cfd = nltk.ConditionalFreqDist(
+        (target, fileid[:4])
+        for fileid in inaugural.fileids()
+        for w in inaugural.words(fileid)
+        for target in ['america', 'citizen']
+        if w.lower().startswith(target))
+    
+cfd.plot()
 
