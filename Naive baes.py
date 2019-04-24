dd92bcd6aa60ffd822191adbcb6b62e4ca06c8f2
Hillel Awaskar <46364984+hillelawaskar@users.noreply.github.com>
2019-04-24 09:00:44 +0530

    NBayes simple classifier

---
+++ Naive baes.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 08:54:33 2019
+
+@author: Irene
+"""
+
+#Import Library of Gaussian Naive Bayes model
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+#assigning predictor and target variables
+x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
+Y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
+
+#Create a Gaussian Classifier
+model = GaussianNB()
+
+# Train the model using the training sets 
+model.fit(x, y)
+
+#Predict Output 
+predicted= model.predict([[1,2],[3,4]])
+print (predicted)
